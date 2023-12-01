acca3b1387408134c545f9b0765c9c7cb2209157
Jack Cappleman <101408144+JackCappleman@users.noreply.github.com>
2023-12-01 21:15:57 +0000

    Combine loops
    - remove the second loop and incorporate it into the first loop
    - remove newly unused variables

--- src/day1/index.ts
+++ src/day1/index.ts
@@ -1,22 +1,12 @@
 import { input } from "./data";
 
-
-
 const array = input.split("\n")
-
-const filter = []
-
+let result = 0
 for(const value of array){
    const numbers = value.match(/\d/g) ?? []
-   if (numbers.length >= 1) filter.push(numbers);
+   if (numbers.length >= 1) {
+      const number = numbers[0] + numbers[numbers.length - 1]
+      result += parseInt(number)
+   };
 }
-
-let result = 0
-for (const value of filter){
-const number = value[0] + value[value.length-1]
-console.log(number)
-result += parseInt(number)
-}
-
 console.log(result);
-
